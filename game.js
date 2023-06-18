25796ed969cd790a73e57cb03a6b2edfa0a4c6e2
skappylmao <honganh6c1@gmail.com>
2023-06-18 21:16:56 +0700

    Added buttons to play game

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+let plrPoints = 0;
+let CPUPoints = 0;
+let plrWon = false;
+
 function getComputerChoice() {
     //generate random number from 0 to 2
     let rand = Math.floor(Math.random() * 3);
@@ -15,41 +19,64 @@ function getComputerChoice() {
 
 
 function playRound(plrSel, cpuSel) {
-    plrSel = plrSel.toLowerCase()
     if (plrSel == "rock" && cpuSel == "paper") {
+        CPUPoints++;
         return "You lose"
+        
     }else if (plrSel == "paper" && cpuSel == "scissors") {
+        CPUPoints++;
         return "You lose"
+        
     }else if (plrSel == "scissors" && cpuSel == "rock") {
+        CPUPoints++;
         return "You lose"
+        
     }else if (plrSel == cpuSel) {
+        plrPoints++;
+        CPUPoints++;
         return "It's a tie"
     }else {
+        plrPoints++;
         return "You win"
+        
     }
 }
 
-function game(){
-    for (let i = 0; i < 5;i++) {
-        let playerSelection = getUserInput()
-        let cpuSelection = getComputerChoice()
-        alert(playRound(playerSelection, cpuSelection))
-    }
-}
+function game(plrSel){
+        let playerSelection = plrSel;
+        let cpuSelection = getComputerChoice();
+        console.log(playRound(playerSelection, cpuSelection));
 
-function getUserInput() {
-    let playerSelection = ""
-    let gotten = false;
-    while (gotten == false) {
-        playerSelection = prompt("rock, paper or scissors?")
-        playerSelection = playerSelection.toLowerCase()
-        if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
-            gotten = true;
-        }else{
-            alert("invalid input")
+        document.querySelector("#plrPoints").innerHTML = plrPoints;
+        document.querySelector("#cpuPoints").innerHTML = CPUPoints;
+        if (plrPoints + CPUPoints >= 5) {
+            let wonStatus = document.createElement("p");
+            if (plrPoints > CPUPoints) {
+                plrWon = true;
+                wonStatus.innerText = "Player Won!";
+                document.querySelector("#results").appendChild(wonStatus);
+            }else if (plrPoints < CPUPoints){
+                wonStatus.innerText = "Computer Won!";
+                document.querySelector("#results").appendChild(wonStatus);
+            }else {
+                wonStatus.innerText = "It's a tie!";
+                document.querySelector("#results").appendChild(wonStatus);
+            }
+            
         }
-    }
-    return playerSelection;
 }
 
-game();
+rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", function(){
+    game("rock");
+})
+
+paper = document.querySelector("#paper");
+paper.addEventListener("click", function() {
+    game("paper");
+})
+
+scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", function() {
+    game("scissors");
+})
